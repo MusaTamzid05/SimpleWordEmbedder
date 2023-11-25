288be4041929ed6e91f910e3c3cf1afa024d6379
MusaTamzid05 <tamzid101@gmail.com>
2023-11-25 14:58:57 +0600

    Add corpus uploading feature

--- server.py
+++ server.py
@@ -1,10 +1,48 @@
 from flask import Flask
+from flask import request
+from flask import redirect
+from flask import url_for
 from flask import render_template
+from flask import flash
+from werkzeug.utils import secure_filename
+
+import os
 
 app = Flask(__name__)
+app.secret_key = "this is the world best secret key"
 
-@app.route("/", methods=["GET"])
+CORPUS_UPLOAD_DIR = "corpus_data"
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    if request.method == "POST":
+        uploaded_file = request.files["file"]
+        if uploaded_file.filename.endswith("txt") == False:
+            flash("Only txt file are allowed")
+
+        else:
+            filename = secure_filename(uploaded_file.filename)
+            upload_dir_path = os.path.join(os.getcwd(), CORPUS_UPLOAD_DIR)
+
+            if os.path.isdir(upload_dir_path) == False:
+                os.mkdir(upload_dir_path)
+
+
+            uploaded_file.save(
+                    os.path.join(upload_dir_path, filename)
+                    )
+            flash(f"{filename} uploaded")
+
+
+
+
+
+        return redirect(url_for("home"))
+
+
+
+
+
     return render_template("home.html")
 
 
